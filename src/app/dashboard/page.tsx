d67ef6938f45cbe2758a6db27728fe724e6f8f0f
AshwinAnand868 <anandashwin868@gmail.com>
2024-04-14 23:53:06 -0300

    Add initial files for the project

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,12 @@
+import { FC } from 'react'
+import Button from '../components/ui/Button'
+
+interface pageProps {
+  
+}
+
+const page: FC<pageProps> = ({}) => {
+  return <Button>hello</Button>
+}
+
+export default page
