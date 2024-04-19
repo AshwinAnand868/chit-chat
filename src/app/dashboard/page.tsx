34db0f691c26a76e0d34dfa2ad692132ff202ccf
AshwinAnand868 <anandashwin868@gmail.com>
2024-04-18 22:46:17 -0300

    Add implementation for Google OAuth

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,12 +1,19 @@
-import { FC } from 'react'
-import Button from '../components/ui/Button'
+import { authOptions } from '@/lib/auth';
+import { getServerSession } from 'next-auth';
 
 interface pageProps {
   
 }
 
-const page: FC<pageProps> = ({}) => {
-  return <Button>hello</Button>
+const page = async ({}) => {
+  const session = await getServerSession(authOptions);
+
+
+
+  return <pre>
+    {JSON.stringify(session)}
+  </pre>
+
 }
 
-export default page
+export default page;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,7 @@
-import db from "@/lib/db";
+import Button from "./components/ui/Button";
 
 export default async function Home() {
-
-  await db.set('hello', 'hello');
-
   return (
-    <div className="text-red-500">Hello World</div>
+    <Button variant='ghost'>Hello</Button>
   );
 }
