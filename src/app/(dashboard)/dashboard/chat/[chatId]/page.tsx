fc0970206913ea7a17921cfe24d9d59156f7371f
AshwinAnand868 <anandashwin868@gmail.com>
2024-05-12 18:50:09 -0300

    Show user profile image for the last message sent or received

--- src/app/(dashboard)/dashboard/chat/[chatId]/page.tsx
+++ src/app/(dashboard)/dashboard/chat/[chatId]/page.tsx
@@ -93,7 +93,7 @@ const page: FC<PageProps> = async ({ params }: PageProps) => {
       </div>
     </div>
 
-    <Messages initialMessages={messagesForGivenChatId} sessionId={session.user.id}/>
+    <Messages chatPartner={chatPartner} sessionImg={session.user.image} initialMessages={messagesForGivenChatId} sessionId={session.user.id}/>
     <ChatInput chatPartner={chatPartner} chatId={chatId}/>
 
   </div>
--- src/app/components/Messages.tsx
+++ src/app/components/Messages.tsx
@@ -3,16 +3,21 @@
 import { cn } from '@/lib/utils';
 import { Message } from '@/lib/validators/message';
 import { format } from 'date-fns';
+import Image from 'next/image';
 import { FC, useRef, useState } from 'react';
 
 interface MessagesProps {
     initialMessages: Message[],
-    sessionId: string
+    sessionId: string,
+    sessionImg: string | null | undefined,
+    chatPartner: User
 }
 
 const Messages: FC<MessagesProps> = ({ 
     initialMessages,
-    sessionId
+    sessionId,
+    sessionImg,
+    chatPartner
  }) => {
 
     const [messages, setMessages] = useState<Message[]>(initialMessages);
@@ -66,6 +71,22 @@ const Messages: FC<MessagesProps> = ({
                                         {formatTimestamp(message.timestamp)}
                                     </span>
                                 </span>
+                            </div>
+
+                            <div className={cn('relative w-6 h-6', {
+                                'order-2': isCurrentUser,
+                                'order-1': !isCurrentUser,
+                                'invisible': hasNextMessageFromSameUser,
+                            })}>
+                                <Image
+                                    fill
+                                    src={
+                                        isCurrentUser ? (sessionImg as string) : chatPartner.image
+                                    }
+                                    alt='Profile picture'
+                                    referrerPolicy='no-referrer'
+                                    className='rounded-full'
+                                />
                             </div>
                         </div>
                     </div>
