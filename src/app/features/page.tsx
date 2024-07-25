7342344cd536be29cd26ee9418c053d4e90960fc
AshwinAnand868 <anandashwin868@gmail.com>
2024-07-25 15:20:28 -0300

    Remove the features dev content

--- src/app/features/page.tsx
+++ src/app/features/page.tsx
@@ -1,28 +1,31 @@
 
 const page = () => {
   return (
-    <div className="flex justify-center items-center mt-10">
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-10">
-            <div className="bg-red-500 w-[300px] h-[300px]">
+    <section>
+        
+    </section>
+    // <div className="flex justify-center items-center mt-10">
+    //     <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-10">
+    //         <div className="bg-red-500 w-[300px] h-[300px]">
 
-            </div>
-            <div className="bg-red-500 w-[300px] h-[300px]">
+    //         </div>
+    //         <div className="bg-red-500 w-[300px] h-[300px]">
 
-            </div>
-            <div className="bg-red-500 w-[300px] h-[300px]">
+    //         </div>
+    //         <div className="bg-red-500 w-[300px] h-[300px]">
 
-            </div>
-            <div className="bg-red-500 w-[300px] h-[300px]">
+    //         </div>
+    //         <div className="bg-red-500 w-[300px] h-[300px]">
 
-            </div>
-            <div className="bg-red-500 w-[300px] h-[300px]">
+    //         </div>
+    //         <div className="bg-red-500 w-[300px] h-[300px]">
 
-            </div>
-            <div className="bg-red-500 w-[300px] h-[300px]">
+    //         </div>
+    //         <div className="bg-red-500 w-[300px] h-[300px]">
 
-            </div>
-        </div>
-    </div>
+    //         </div>
+    //     </div>
+    // </div>
   )
 }
 
