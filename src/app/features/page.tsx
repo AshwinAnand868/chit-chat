99ea856e738eb0f88bdaa2a9f6d68c211679801a
AshwinAnand868 <anandashwin868@gmail.com>
2024-07-26 00:36:05 -0300

    Finish features page basic design

--- src/app/features/page.tsx
+++ src/app/features/page.tsx
@@ -1,8 +1,151 @@
+"use client";
+
+import { Transition, TransitionChild } from "@headlessui/react";
+import {
+    FaComments,
+    FaLock,
+    FaMoon,
+    FaPhotoVideo,
+    FaSmile,
+    FaVideo,
+} from "react-icons/fa";
+
+import { useState } from "react";
+import { IconType } from "react-icons/lib";
+import { InView } from "react-intersection-observer";
+
+type Feature = {
+  icon: IconType;
+  heading: string;
+  description: string;
+};
+
+const features: Feature[] = [
+  {
+    icon: FaPhotoVideo,
+    heading: "Media Sharing",
+    description:
+      "Share photos and documents effortlessly. Keep your conversations lively with multimedia content.",
+  },
+  {
+    icon: FaLock,
+    heading: "Secure Messaging",
+    description:
+      "End-to-end encryption for your messages to keep your conversations private and secure.",
+  },
+  {
+    icon: FaVideo,
+    heading: "Video Calls",
+    description: "High-quality video calls to connect face-to-face.",
+  },
+  {
+    icon: FaSmile,
+    heading: "Custom Emojis",
+    description: "Express yourself with custom emojis.",
+  },
+  {
+    icon: FaMoon,
+    heading: "Dark Mode",
+    description: "A night-time mode to reduce eye strain.",
+  },
+];
 
 const page = () => {
+  const [lastCardInView, setLastCardInView] = useState<boolean>(false);
+
   return (
-    <section>
-        
+    <section id="features" className="py-10">
+      <div className="container mx-auto px-6">
+        <h2 className="text-xl sm:text-2xl md:text-4xl font-bold text-center mb-10 sm:mb-16 text-red-400">
+          Everything you need for effortless conversations
+        </h2>
+
+        <div className="mt-5 sm:mt-10">
+          <h3 className="text-xl sm:text-2xl md:text-3xl font-bold text-center mb-5 text-blue-500">
+            Current Features
+          </h3>
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+            <div>
+              <div className="opactiy-100 translate-y-0">
+                <div className="flex gap-4 p-6 bg-white shadow-md rounded-lg hover:shadow-xl transition-shadow duration-300 glow-effect">
+                  <FaComments className="h-24 w-24 text-blue-500 mr-4" />
+                  <div>
+                    <h3 className="text-xl font-semibold mb-2">
+                      Text Messaging
+                    </h3>
+                    <p className="text-gray-600">
+                      Enjoy seamless and reliable text messaging with friends
+                      and family.
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="mt-10">
+          <h3 className="text-xl sm:text-2xl md:text-3xl font-bold text-center mb-7 text-gray-500">
+            Features that may come
+          </h3>
+
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+            {features.map((feature, index) => (
+              <InView
+                key={index}
+                triggerOnce
+                onChange={(inView) => {
+                  if (index == features.length - 1 && inView) {
+                    // Only update lastCardInView state if the last card is in view to prevent premature state updates
+                    if (index === features.length - 1 && inView) {
+                      setLastCardInView(true);
+                    }
+                  }
+                }}
+              >
+                {({ inView, ref }) => (
+                  <div ref={ref}>
+                    <Transition show={inView}>
+                      <TransitionChild
+                        as="div"
+                        enter="ease-in duration-[1000ms] transition-all sm:transition-none"
+                        enterFrom="opacity-0 translate-y-48 sm:translate-y-0 sm:opacity-100"
+                        enterTo="opacity-100 translate-y-0"
+                        // className="w-full "
+                      >
+                        <div className="flex gap-4 p-6 bg-white shadow-md rounded-lg hover:shadow-xl transition-shadow duration-300 glow-effect">
+                          <feature.icon className="h-24 w-24 text-gray-500 mr-4" />
+                          <div>
+                            <h3 className="text-xl font-semibold mb-2">
+                              {feature.heading}
+                            </h3>
+                            <p className="text-gray-600">
+                              {feature.description}
+                            </p>
+                          </div>
+                        </div>
+                      </TransitionChild>
+                    </Transition>
+                  </div>
+                )}
+              </InView>
+
+              // <Transition>
+              //     <TransitionChild>
+
+              //     </TransitionChild>
+
+              // </Transition>
+            ))}
+          </div>
+          <div className="mt-20 text-center text-gray-500 text-sm">
+            <p>
+              Some features listed are for design purposes and may not be
+              available at launch. Stay tuned for updates!
+            </p>
+          </div>
+        </div>
+      </div>
     </section>
     // <div className="flex justify-center items-center mt-10">
     //     <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-10">
@@ -26,7 +169,7 @@ const page = () => {
     //         </div>
     //     </div>
     // </div>
-  )
-}
+  );
+};
 
-export default page
+export default page;
