005faef7bd0dbf17e7259917ad253f8ea553fc1b
AshwinAnand868 <anandashwin868@gmail.com>
2024-08-31 23:04:54 -0400

    Finally made some improvements on video calling feature and adding this for tracking the work

--- src/actions/stream.actions.ts
+++ src/actions/stream.actions.ts
@@ -1,11 +1,8 @@
 "use server";
 
 import { authOptions } from "@/lib/auth";
-import { StreamClient } from "@stream-io/node-sdk";
+import client from "@/lib/streamClient";
 import { getServerSession } from "next-auth";
-
-const apiKey = process.env.NEXT_PUBLIC_STREAM_API_KEY;
-const secret = process.env.STREAM_SECRET_KEY;
 
 export const tokenProvider = async() => {
     const session = await getServerSession(authOptions);
@@ -16,16 +13,11 @@ export const tokenProvider = async() => {
 
     const user = session.user;
 
-    if(!apiKey) throw new Error('No Stream API Key');
-    if(!secret) throw new Error('No Stream Secret');
-
-    const streamClient = new StreamClient(apiKey, secret);
-
     const exp = Math.round(new Date().getTime() / 1000) + 60 * 60;
 
     const issued = Math.floor(Date.now() / 1000) - 60;
 
-    const token = streamClient.createToken(user.id, exp, issued);
+    const token = client.createToken(user.id, exp, issued);
 
     return token;
 }
